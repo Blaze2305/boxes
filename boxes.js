e398bc2ac5b01fe6ca71c25996dafc94c5b69a5c
Blaze2305 <prajpran@gmail.com>
2019-05-13 18:57:05 +0530

    added boxes and basic wave

--- boxes.js
+++ boxes.js
@@ -1,38 +1,72 @@
-let b;
+let b=[];
+
+let n,w;
 
 function setup(){
+  n=5;
+  createCanvas(800,800,WEBGL);
+  w=300/5;
+  for(let j=0;j<n;j++){
+    b[j]=[];
+    for(let k=0;k<n;k++){
+      b[j][k]= new boxes(w*(j+1),100,w*(k+1),100,w-5,0.001*k);
+    }
+  }
 
-createCanvas(800,800,WEBGL);
-b = new boxes(100,100,100,100,50);
 }
 
 
 
 function draw(){
 
-background(0);
-b.show();
-b.update();
-b.wave();
 
+  background(0);
+  //gives the axes at the top left for refrence W=x;R=y;G=z
+  push();
+  box(100);
+  translate(-300,-100,-100);
+  rotateX(PI);
+  stroke(255);
+  background(0);
+  strokeWeight(4);
+  line(0,0,0,100,0,0);
+  stroke(0,255,0);
+  line(0,0,0,0,0,100);
+  stroke(255,0,0);
+  line(0,0,0,0,100,0);
+  pop();
+
+  rotateX(PI);
+  rotateX(-PI/6)
+  translate(-100,-200,-100);
+  for(let i=0;i<n;i++){
+    for(let m=0;m<n;m++){
+      b[i][m].show();
+      b[i][m].update();
+      b[i][m].wave();
+    }
+  }
 }
 
 
 
 class boxes{
 
-  constructor(x,y,z,height,side){
-      this.x=x;
-      this.y=y;
-      this.z=z;
-      this.side=side;
-      this.height=height;
-      this.angle=0;
+  constructor(x,y,z,height,side,offset){
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    this.side=side;
+    this.height=height;
+    this.angle=0;
+    this.offset=offset;
   }
 
   show(){
     push();
-    stroke(255,0,0);
+    strokeWeight(3);
+    fill(0,150,0);
+    stroke(0);
     translate(this.x,this.y,this.z);
     box(this.side,this.height,this.side);
     pop();
@@ -40,6 +74,7 @@ class boxes{
 
   update(){
     this.angle+=0.05;
+    this.angle+=this.offset;
   }
 
   wave(){
